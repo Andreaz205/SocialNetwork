e31640bccc8be72284e6cc718defcf592c5c5291
Andreaz205 <andrei-bazunov@mail.ru>
2022-08-07 17:52:53 +0500

    2 commit

---
+++ src/redux/chat-reducer.ts
@@ -0,0 +1,81 @@
+import {ResultCodeForCapcthaEnum, ResultCodesEnum} from '../api/api'
+import {stopSubmit} from 'redux-form'
+import {authAPI} from '../api/auth-api'
+import {securityAPI} from '../api/security-api'
+import {BaseThunkType, InferActionsTypes} from './redux-store'
+import {Action, Dispatch} from 'redux'
+import {FormAction} from 'redux-form/lib/actions'
+import {chatAPI, ChatMessageAPIType, StatusType} from '../api/chat-api'
+import {v1} from 'uuid'
+
+
+let initialState = {
+    messages: [],
+    status: 'pending'
+}
+
+const chatReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SN/chat/MESSAGES_RECEVIED':
+            return {
+                ...state,
+                messages: [...state.messages, ...action.payload.messages.map( m => ({...m, id: v1() }))]
+                    .filter((m, index, array) => index >= array.length - 100)
+            }
+        case 'SN/chat/STATUS_CHANGED':
+            return {
+                ...state,
+                status: action.payload.status
+            }
+        default:
+            return state
+    }
+}
+
+export const actions = {
+    messagesReceived: (messages) => ({
+        type: 'SN/chat/MESSAGES_RECEVIED', payload: {messages}
+    }),
+    statusChanged: (status) => ({
+        type: 'SN/chat/STATUS_CHANGED', payload: {status}
+    })
+}
+
+let _newMessageHandler: ((messages) => {}) = null
+const newMessageHandlerCreator = (dispatch) => {
+    if (_newMessageHandler === null) {
+        _newMessageHandler = (messages) => {
+            dispatch(actions.messagesReceived(messages))
+        }
+    }
+    return _newMessageHandler
+}
+
+let _statusChangedHandler: ((status) => {}) = null
+const statusChangedHandlerCreator = (dispatch) => {
+    if (_statusChangedHandler === null) {
+        _statusChangedHandler = (status) => {
+            dispatch(actions.statusChanged(status))
+        }
+    }
+    return _statusChangedHandler
+}
+
+export const startMessagesListening = ()=> async (dispatch) => {
+    chatAPI.start()
+    chatAPI.subscribe('messages-received', newMessageHandlerCreator(dispatch))
+    chatAPI.subscribe('status-changed', statusChangedHandlerCreator(dispatch))
+
+}
+export const stopMessagesListening = () => async (dispatch) => {
+    chatAPI.unsubscribe('messages-received', newMessageHandlerCreator(dispatch))
+    chatAPI.unsubscribe('status-changed', statusChangedHandlerCreator(dispatch))
+    chatAPI.stop()
+}
+
+export const sendMessage = (message) => async (dispatch) => {
+    chatAPI.sendMessage(message)
+}
+
+
+export default chatReducer
